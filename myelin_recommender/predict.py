b1a6e26a0d1d7d0b0eb4fdac6c181fa570870d0f
tamasjambor <jamborta@gmail.com>
2019-01-17 23:41:55 +0000

    predict example

--- myelin_recommender/predict.py
+++ myelin_recommender/predict.py
@@ -8,7 +8,7 @@ data_path = os.environ.get('DATA_PATH') or '/tmp/data/'
 mnist_images = np.load(os.path.join(data_path, "train_data.npy"))
 
 address = "35.234.144.51:80"
-endpoint = "ml-recommender-all25-4224909824-2539877511"
+endpoint = "ml-recommender-all38-3300436330-2539877511"
 
 url = "http://%s/%s/predict" % (address, endpoint)
 data = [[10, 2], [10, 3]]
@@ -17,8 +17,6 @@ jsondata = json.dumps({"data": {"ndarray": data}})
 payload = {'json': jsondata}
 
 response = requests.post(url, data=payload, headers={'User-Agent': 'test'})
-print(response.status_code)
-print(response.text)
-# json_data = json.loads(response.text)
-# prediction = json_data["data"]["ndarray"]
-# print(prediction)
+json_data = json.loads(response.text)
+prediction = json_data["data"]["ndarray"]
+print(prediction)
