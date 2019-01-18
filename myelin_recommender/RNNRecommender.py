5b4e73ce0ec2c8f967ff8ade828182c4bd4da757
tamasjambor <jamborta@gmail.com>
2019-01-18 00:20:36 +0000

    adding metric

--- myelin_recommender/RNNRecommender.py
+++ myelin_recommender/RNNRecommender.py
@@ -1,4 +1,5 @@
 import os
+
 os.environ['KERAS_BACKEND'] = 'theano'
 os.environ['THEANO_FLAGS'] = "blas.ldflags=\"-L/usr/lib/openblas-base -lopenblas\""
 
@@ -6,8 +7,10 @@ from myelin_model.cf_model import CFModel
 from myelin_model.utils import load_obj
 import numpy as np
 import sys
+from myelin import metric
 
 model_path = os.environ.get('MODEL_PATH') or '/tmp/model/'
+
 
 class RNNRecommender(object):
 	def __init__(self):
@@ -17,6 +20,7 @@ class RNNRecommender(object):
 		self.trained_model.load_weights(os.path.join(model_path, 'weights.h5'))
 		self.trained_model._make_predict_function()
 		self.class_names = ["class:rating"]
+		self.c = metric.MetricClient()
 
 	def predict_rating(self, row):
 		user_id = row[0]
@@ -24,9 +28,10 @@ class RNNRecommender(object):
 		return self.trained_model.rate(user_id - 1, movie_id - 1)
 
 	def predict(self, X, feature_names):
-		print("Getting this from crap seldon", file=sys.stdout)
-		print(X, file=sys.stdout)
-		print("Getting this from crap seldon", file=sys.stderr)
 		print(X, file=sys.stderr)
 		predictions = np.apply_along_axis(self.predict_rating, axis=1, arr=X)
 		return predictions
+
+	def send_feedback(self, features, feature_names, reward, truth):
+		print("Posting reward: %s" % reward)
+		self.c.post_update("recommender_deploy_accuracy", reward)
